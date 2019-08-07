2d71ae396e02cdf4e90e679ac1012ea02bc86ccb
Robin Siep <RobinSiep1@gmail.com>
2019-08-07 15:46:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 
 
 setup(
-    name='fakerediss',
-    version='1.0.4',
+    name='fakeredis',
+    version='1.0.5',
     description="Fake implementation of redis API for testing purposes.",
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        'README.rst')).read(),
